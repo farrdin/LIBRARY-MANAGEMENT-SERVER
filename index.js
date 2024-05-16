d6932e47b2b80c4830fb79597b364d2fec8fe04d
Fardin Ahmed Alif <Alif.Ahmed44@gmail.com>
2024-05-17 02:22:39 +0600

    update method added using PATCH

--- index.js
+++ index.js
@@ -24,16 +24,15 @@ async function run() {
     await client.connect();
 
     const AllData = client.db("PRB9-A11").collection("All");
-    const Borrowed = client.db("PRB9-A11").collection("Borrowed");
-    const Add = client.db("PRB9-A11").collection("Add");
-
     // All Data
     app.get("/all", async (req, res) => {
       const cursor = AllData.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    // Borrow Book
+
+    const Borrowed = client.db("PRB9-A11").collection("Borrowed");
+    // BorrowED
     app.get("/borrowed", async (req, res) => {
       let query = {};
       if (req.query?.email) {
@@ -56,20 +55,39 @@ async function run() {
       res.send(result);
     });
 
-    //  Add Books
-  app.get("/add", async (req, res) => {
-    let query = {};
-    if (req.query?.email) {
-      query = { email: req.query?.email };
-    }
-    const result = await Add.find(query).toArray();
-    res.send(result);
-  });
+    const Add = client.db("PRB9-A11").collection("Add");
+    //  Add
+    app.get("/add", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query?.email };
+      }
+      const result = await Add.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/add", async (req, res) => {
       const add = req.body;
       const result = await Add.insertOne(add);
       res.send(result);
+    });
+    app.patch("/add/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateBook = req.body;
+      console.log(updateBook);
+      const updateDoc = {
+        $set: updateBook,
+      };
+      try {
+        const result = await Add.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error("Error updating document:", error);
+        res
+          .status(500)
+          .send({ error: "An error occurred while updating the document." });
+      }
     });
 
     await client.db("admin").command({ ping: 1 });
